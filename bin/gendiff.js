cae0d076ba5ffd359579de199008ed2a569bfeee
Sergey Zhukov <sergtmb@list.ru>
2021-06-17 23:46:24 +0300

    stage 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,10 @@
 import { Command } from 'commander';
 const program = new Command();
 
-program.description('Compares two configuration files and shows a difference.');
-
-program.version('0.0.1');
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);
